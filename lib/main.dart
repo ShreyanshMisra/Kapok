8d25fd1b0e99cb0f9c62588ca411e78e8640fb20
Atonbara-Adora <the.atonbara@gmail.com>
2024-11-11 16:23:52 -0500

    Sign Up Page
    sign up page made according to specifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kapok_new/pages/auth_page/login_page.dart';
+import 'package:kapok_new/pages/auth_page/sign_up_page.dart';
 import 'package:kapok_new/pages/home_screens/create_task_page.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: CreateTaskPage(),  //change MyApp to your page name to see your page output
+      home: SignUpPage(),  //change MyApp to your page name to see your page output
     );
   }
 }
